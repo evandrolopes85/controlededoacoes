1a9b5ef12cbd95c72b0219b3f0b013fcb890b80c
evandrolopes85 <evandrocwd@gmail.com>
2022-06-20 03:42:59 -0300

    Tentado realizar teste, erro no migration

---
+++ src/test/java/io/github/evandrolopes85/doacoes/controller/PessoaControllerTest.java
@@ -0,0 +1,25 @@
+package io.github.evandrolopes85.doacoes.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+//@AutoConfigureMockMvc
+public class PessoaControllerTest {  //extends DoacoesApplicationTests{
+	
+//	@Autowired
+//	private MockMvc mock;
+//	
+//	@Test
+//	public void recuperarTodasPessoas_RetornarStatusCode200() throws Exception{
+//		mock.perform(
+//				MockMvcRequestBuilders.get("/pessoa")
+//				.contentType(MediaType.APPLICATION_JSON)
+//				)
+//		.andExpect(MockMvcResultMatchers.status().isOk())
+//		;
+//	}
+}
